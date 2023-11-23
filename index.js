d27509a981571b69c8e0ca986a28a871c3634f16
Maxym <miksimmiks@gmail.com>
2023-11-23 12:58:26 +0200

    fixed

--- index.js
+++ index.js
@@ -23,17 +23,31 @@ function writefile() {
   fs.writeFileSync(filename, JSON.stringify(notes), 'utf8');
 }
 
-app.get('/', (req, res) => {
-  res.send('The service is running. Congratulations!');
+app.get('/notes', (req, res) => {
+  res.json(notes);
 });
 
 app.get('/UploadForm.html', (req, res) => {
   res.sendFile(__dirname + '/static/UploadForm.html');
 });
 
-app.get('/notes', (req, res) => {
-  res.json(notes);
-});
+app.post('/upload', (req, res) => {
+  const note_name = req.body.note_name;
+  const note = req.body.note;
+  const existing = notes.find(note => note.note_name === note_name);
+
+  if (existing) {
+    res.status(400).send("This note already exist");
+  } else {
+    notes.push({ note_name: note_name, note: note });
+
+  writefile();
+
+
+    res.status(201).send("Created");
+  } 
+}); 
+
 
 app.get('/notes/:note_name', (req, res) => {
   const note_name = req.params.note_name;
@@ -46,31 +60,12 @@ app.get('/notes/:note_name', (req, res) => {
   }
 });
 
-app.post('/upload', (req, res) => {
-  const note_name = req.body.note_name;
-  const note = req.body.note;
-  const existing = notes.find(note => note.note_name === note_name);
-
-  if (existing) {
-    res.status(400).send("Bad Request");
-  } else {
-    notes.push({ note_name: note_name, note: note });
-
-  writefile();
-
-    console.log('Current state of notes:', notes);
-
-    res.status(201).send("OK");
-  } 
-}); 
-
 app.put("/notes/:noteName", (req, res) => {
   const noteName = req.params.noteName;
   const noteIndex = notes.findIndex(note => note.note_name === noteName);
 
   if (noteIndex !== -1) {
     notes[noteIndex].note = req.body;
-    console.log('Поточний стан нотаток:', req.body);
     res.sendStatus(200);
   } else {
     res.sendStatus(404);
@@ -85,7 +80,7 @@ app.delete('/notes/:note_name', (req, res) => {
   if (noteIndex !== -1) {
     notes.splice(noteIndex, 1);
     writefile()
-    res.status(200).send('OK');
+    res.status(200).send('Note deleted');
   } else {
     res.status(404).send('Not Found');
   }
